d5c5527ee08962232bd101598f848366e4548fe1
Sahil S <sahils.8877@gmail.com>
2025-04-27 13:42:48 +0100

    changes

--- Day 1/BandNameGen.py
+++
@@ -1,4 +0,0 @@
-print("Band Name Generator")
-cityName = input("What is the name of the city you grew in ?\n")
-petName = input("What is the name of your pet?\n")
-print("Your Band Name is :", cityName + " " + petName)
