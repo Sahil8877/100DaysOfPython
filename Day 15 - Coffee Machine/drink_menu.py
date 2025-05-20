b2b4c74af93209311a603ec66d496a10a5c4e8d2
Sahil S <sahils.8877@gmail.com>
2025-05-20 14:52:21 +0100

    Day 15

---
+++ Day 15 - Coffee Machine/drink_menu.py
@@ -0,0 +1,40 @@
+MENU = {
+    "Espresso": {
+        "ingredients": {
+            "coffee": 9,     # grams (single shot)
+            "water": 30      # milliliters
+        },
+        "cost":2.40
+    },
+    "Latte": {
+        "ingredients": {
+            "coffee": 9,     # grams (single shot)
+            "milk": 150,     # milliliters
+            "water": 30      # milliliters (espresso shot)
+        },
+        "cost":2.50
+    },
+    "Cappuccino": {
+        "ingredients": {
+            "coffee": 9,     # grams (single shot)
+            "milk":  60,     # milliliters (steamed milk)
+            "water": 30      # milliliters (espresso shot)
+        },
+        "cost":2.80
+    },
+    "Hot Chocolate": {
+        "ingredients": {
+            "cocoa": 15,     # grams unsweetened cocoa powder
+            "milk": 240,     # milliliters
+            "sugar": 10      # grams
+        },
+        "cost":3.10
+    },
+    "Tea": {
+        "ingredients": {
+            "tea": 2,        # grams (≈1 tea bag)
+            "water": 240     # milliliters
+        },
+        "cost":2.0
+    }
+}
