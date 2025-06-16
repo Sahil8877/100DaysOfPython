d1d5d96a7071d1a76d43739e5a807dad13463fba
Sahil S <sahils.8877@gmail.com>
2025-06-16 03:32:58 +0100

    Day 23 - Turtle Crossing

---
+++ Day 23 /main.py
@@ -0,0 +1,46 @@
+from turtle import Screen
+import time
+from car_manager import Car_Manager
+from player import Player
+
+screen = Screen()
+#set bg color for window
+screen.bgcolor("black")
+#set color mode to r,g,b 
+screen.colormode(255)
+screen.setup(width=600,height=600)
+screen.title("Turtle Crossing")
+#turn off animations
+screen.tracer(0)
+#car_manager class instance
+cars = Car_Manager()
+#player class instance
+player = Player(screen)
+
+is_game_over = False
+
+#generate cars by calling car_generator method
+cars.car_generator()
+
+#listening to events
+screen.listen()
+#key events to control the turtle
+screen.onkeypress(fun=player.player_move_up,key="Up")
+screen.onkeypress(fun=player.player_move_down,key="Down")
+
+while not is_game_over:
+    time.sleep(0.5 * player.GAME_SPEED)
+
+    if player.check_collision(cars.list_of_cars):
+        #if collision detected then game ends
+        is_game_over = True
+        
+    else:
+        cars.car_moving()
+
+    screen.update()
+
+    
+
+    
+        
---
+++ Day 23 /player.py
@@ -0,0 +1,55 @@
+from turtle import Turtle
+import time
+from scoreboard import Scoreboard
+
+class Player():
+    def __init__(self,screen):
+        """
+        Player inherits from scoreboard
+        defines game speed and turtle object as player
+        """
+        self.scoreboard = Scoreboard(screen)
+        self.player = Turtle(shape="turtle")
+        self.player.setheading(90)
+        self.player.penup()
+        self.player.color("white")
+        self.player.goto(0,-250)
+        self.GAME_SPEED = 0.05
+        
+    
+    def player_move_up(self):
+        """
+        method to move the player up 10 steps
+        """
+        if self.player.ycor() < 250:
+            self.player.forward(10)
+    
+    def player_move_down(self):
+        """
+        method to move the player down 10 steps
+        """
+        if self.player.ycor() > -250:
+            self.player.backward(10)
+
+    def check_collision(self,cars):
+        """
+        method to track object and player collisions
+        if collision is detected display GAME OVER and return True 
+        else if the player crosses 250 on y axis, reset player position, 
+        increment game speed and return none
+        """
+        for car in cars:
+            if car.distance(self.player) <= 25:
+                
+                print("collision detected")
+                self.scoreboard.game_over()
+                time.sleep(2)
+                return True
+            if self.player.ycor() == 250: 
+                self.scoreboard.add_score()
+                self.player.goto(0,-250)
+                self.GAME_SPEED -= 0.005
+                return None
+    
+    
+        
