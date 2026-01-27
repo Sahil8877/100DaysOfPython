c0ec6e52e90207e4c4e7694d1f91ce657b078a93
Sahil Suryavanshi <sahils.8877@gmail.com>
2026-01-27 21:08:55 +0000

    Updated for better readability

--- Day 32 - Email Sender/main.py
+++ Day 32 - Email Sender/main.py
@@ -14,8 +14,11 @@ def parse_recipients(recepients_list):
     return receiver_dict
 
 def mail_for_india():
-    generate_quote = quote('Growth Motivation',limit=20)
     receiver_dict = parse_recipients(recipients_list_ind)
+    send_email(receiver_dict)
+    
+def send_email(receiver_dict):
+    generate_quote = quote('Growth Motivation',limit=20)
     for receiver in receiver_dict:
         random_quote = random.choice(generate_quote)
         message = f"Subject:Hi {receiver}, Todays Quote From {random_quote['author']}.\n\nYour Daily Motivation :\n\n\n{random_quote['quote']}\n\n\nPlease do not reply to this email."
@@ -25,18 +28,10 @@ def mail_for_india():
             conn.sendmail(from_addr=sender, to_addrs=receiver_dict[receiver], msg=message.encode("utf-8"))
 
 def mail_for_uk():
-    generate_quote = quote('Growth Motivation',limit=20)
     receiver_dict = parse_recipients(recipients_list_uk)
-    for receiver in receiver_dict:
-        random_quote = random.choice(generate_quote)
-        message = f"Subject:Hi {receiver}, Todays Quote From {random_quote['author']}.\n\nYour Daily Motivation :\n\n\n{random_quote['quote']}\n\n\nPlease do not reply to this email."
-        with smtplib.SMTP(host='smtp.gmail.com') as conn:
-            conn.starttls()
-            conn.login(user=sender,password=password)
-            conn.sendmail(from_addr=sender, to_addrs=receiver_dict[receiver], msg=message.encode("utf-8"))
+    send_email(receiver_dict)
 
 now_utc = datetime.datetime.now(pytz.utc)
-
 curr_time_india = now_utc.astimezone(pytz.timezone("Asia/Kolkata"))
 curr_time_uk = now_utc.astimezone(pytz.timezone("Europe/London"))
 
