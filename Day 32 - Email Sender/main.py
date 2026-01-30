905392dbe9521a9b6243592521ab34e7f36e0261
SahilD <71732134+Sahil8877@users.noreply.github.com>
2026-01-30 19:28:41 +0000

    Change email subject line to 'Monday Motivation'

--- Day 32 - Email Sender/main.py
+++ Day 32 - Email Sender/main.py
@@ -54,7 +54,7 @@ def send_email(receiver_dict):
         conn.login(user=sender,password=password)
         for receiver in receiver_dict:
             random_quote = random.choice(generate_quote)
-            message = f"Subject:Hi {receiver}, Todays Quote From {random_quote['author']}.\n\nYour Daily Motivation :\n\n\n{random_quote['quote']}\n\n\nPlease do not reply to this email."
+            message = f"Subject:Hi {receiver}, Todays Quote From {random_quote['author']}.\n\nYour Monday Motivation :\n\n\n{random_quote['quote']}\n\n\nPlease do not reply to this email."
             conn.sendmail(from_addr=sender, to_addrs=receiver_dict[receiver], msg=message.encode("utf-8"))
 
 if curr_time_uk >= TARGET_TIME_UK and last_sent_uk != str(curr_time_uk.date()):
