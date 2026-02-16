da420e7b8ef93058d9f0acbe827d63fe293b2540
SahilD <71732134+Sahil8877@users.noreply.github.com>
2026-02-16 06:57:18 +0000

    Update main.py

--- Day 35 - API Auth/main.py
+++ Day 35 - API Auth/main.py
@@ -20,7 +20,7 @@ def check_weather_for_rain():
 
     for minute in minutely_data[:30]:
         print(minute['precipitation'])
-        if minute['precipitation'] >= 0.1:
+        #if minute['precipitation'] >= 0.1:
             rain_level_mm = minute['precipitation'] #for mm/hr
             print('rain_level_mm :',rain_level_mm)
             sum_of_precipitation.append(rain_level_mm)
