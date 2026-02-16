cb448ee81cd9fd581a3f7afec6667cb5e5d96026
Sahil <sahils.8877@gmail.com>
2026-02-16 19:39:19 +0000

    Refactor weather check logic by uncommenting code and improving readability

--- Day 35 - API Auth/main.py
+++ Day 35 - API Auth/main.py
@@ -2,44 +2,45 @@ import data
 import statistics
 
 def check_weather_for_rain():
-
-    # fetch_weather_data = data.WeatherData()
-    # minutely_data = fetch_weather_data.minutely_forecast()
-    # sum_of_precipitation = []
-    # send_alert = False
-    # light_rain_count = 0
-    # moderate_rain_count = 0
-    # heavy_rain_count = 0
     previous_state = None
-
-    # try:
-    #     with open('previous_state.txt','r') as file:
-    #         previous_state = file.read()
-    # except FileNotFoundError as exception:
-    #     print("File doesnt exist!")
-
-    # for minute in minutely_data[:30]:
-    #     print(minute['precipitation'])
-    #     if minute['precipitation'] >= 0.1:
-    #         rain_level_mm = minute['precipitation'] #for mm/hr
-    #         print('rain_level_mm :',rain_level_mm)
-    #         sum_of_precipitation.append(rain_level_mm)
-    #         if 1 <= rain_level_mm < 3:
-    #             light_rain_count += 1
-    #         elif 3 <= rain_level_mm < 8:
-    #             moderate_rain_count += 1
-    #         elif rain_level_mm >= 8:
-    #             heavy_rain_count += 1
-
-    # if len(sum_of_precipitation) > 0:
-    #     avg_intensity_over_next30 = statistics.median(sum_of_precipitation)
-    # else:
-    #     avg_intensity_over_next30 = 0
-
-    # pop_value = fetch_weather_data.hourly_pop()
-
-    # print(f"light_rain_count = {light_rain_count}\nmoderate_rain_count = {moderate_rain_count}\nheavy_rain_count = {heavy_rain_count}\npop = {pop_value['pop']}\navg_intensity_over_next30 = {avg_intensity_over_next30}")
+    """
+    fetch_weather_data = data.WeatherData()
+    minutely_data = fetch_weather_data.minutely_forecast()
+    sum_of_precipitation = []
+    send_alert = False
+    light_rain_count = 0
+    moderate_rain_count = 0
+    heavy_rain_count = 0
     
+
+    try:
+        with open('previous_state.txt','r') as file:
+            previous_state = file.read()
+    except FileNotFoundError as exception:
+        print("File doesnt exist!")
+
+    for minute in minutely_data[:30]:
+        print(minute['precipitation'])
+        if minute['precipitation'] >= 0.1:
+            rain_level_mm = minute['precipitation'] #for mm/hr
+            print('rain_level_mm :',rain_level_mm)
+            sum_of_precipitation.append(rain_level_mm)
+            if 1 <= rain_level_mm < 3:
+                light_rain_count += 1
+            elif 3 <= rain_level_mm < 8:
+                moderate_rain_count += 1
+            elif rain_level_mm >= 8:
+                heavy_rain_count += 1
+
+    if len(sum_of_precipitation) > 0:
+        avg_intensity_over_next30 = statistics.median(sum_of_precipitation)
+    else:
+        avg_intensity_over_next30 = 0
+
+    pop_value = fetch_weather_data.hourly_pop()
+
+    print(f"light_rain_count = {light_rain_count}\nmoderate_rain_count = {moderate_rain_count}\nheavy_rain_count = {heavy_rain_count}\npop = {pop_value['pop']}\navg_intensity_over_next30 = {avg_intensity_over_next30}")
+    """
     light_rain_count = 5
     moderate_rain_count = 12
     heavy_rain_count = 0
