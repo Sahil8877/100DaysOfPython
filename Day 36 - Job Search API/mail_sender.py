f25cbbc9a9d9dc0a552ae778dfb336fc9fda59b4
Sahil <sahils.8877@gmail.com>
2026-02-19 00:15:12 +0000

    Refactor job parsing function and add email sending feature
    - Changed the function name from `parse_dev_jobs` to `parse_jobs` to make it more generic and updated the data fetching method to accept a query string.
    - Added a new module `mail_sender.py` to handle email sending, including fetching job data and preparing email messages for recipients based on predefined job queries.
    - Introduced a new module `msg_builder.py` to format job data into a message for email content.
    - Implemented environment variable loading for email credentials and recipient lists.

---
+++ Day 36 - Job Search API/mail_sender.py
@@ -0,0 +1,86 @@
+import msg_builder
+import smtplib
+import os
+from email.message import EmailMessage
+from dotenv import load_dotenv
+load_dotenv()
+
+job_queries = [
+    {
+        '0' : 
+        {
+            "query":"python developer jobs in uk",
+            "page":"1",
+            "num_pages":"1",
+            "country":"uk",
+            "date_posted":"all",
+            "employment_types":"FULLTIME,INTERN",
+            "job_requirements":"under_3_years_experience"
+        }
+    },
+    {
+        '1' : 
+        {
+            "query":"pharmacy research jobs in uk",
+            "page":"1",
+            "num_pages":"1",
+            "country":"uk",
+            "date_posted":"all",
+            "employment_types":"FULLTIME,INTERN",
+            "job_requirements":"under_3_years_experience"
+        }
+    },
+    {
+        '2':
+        {
+            "query":"marketing jobs in uk",
+            "page":"1",
+            "num_pages":"1",
+            "country":"uk",
+            "date_posted":"all",
+            "employment_types":"FULLTIME,INTERN",
+            "job_requirements":"under_3_years_experience"
+        }
+    }
+]
+def fetch_email_data():
+    sender = os.getenv("SENDER_EMAIL")
+    password = os.getenv("SENDER_EMAIL_PASS")
+    recipients_list_uk_for_jobs = os.getenv("RECIPIENTS_LIST_UK_FOR_JOBS")
+    return sender,password,recipients_list_uk_for_jobs
+
+def parse_recipients(recipients_list):
+    receiver_dict = {}
+    for key in recipients_list.split(','):
+        name, query_num, email = key.split(':')
+        receiver_dict[name.strip(" '' ")] = query_num.strip(), email.strip()
+    return receiver_dict
+
+def send_mail():
+    sender, password, recipients_list_uk_for_jobs = fetch_email_data()
+    receiver_dict = parse_recipients(recipients_list_uk_for_jobs)
+    
+    with smtplib.SMTP(host='smtp.gmail.com') as conn:
+        conn.starttls()
+        conn.login(user=sender,password=password)
+        
+        for receiver in receiver_dict:
+            msg = EmailMessage()
+            msg['From'] = sender
+            msg['To'] = receiver_dict[receiver][1]
+            msg['Subject'] = f"Morning {receiver}, Check out your fresh job postings!"
+            msg.set_content(f"{prepare_jobs(receiver_dict[receiver][0])}")
+            conn.send_message(msg)
+            print(f"Email sent to {receiver}")
+
+def prepare_jobs(query_num):
+    for query in job_queries:
+        for index in query:
+            if query_num == index:
+                message = msg_builder.prepare_msg(query[index])
+                break
+    return message
+                
+send_mail()
+  
+
