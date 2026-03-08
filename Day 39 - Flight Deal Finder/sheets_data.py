f7596a2f6db9b7e8c8430413f33ccc8bec126e48
Sahil <sahils.8877@gmail.com>
2026-03-08 03:09:34 +0000

    Refactor sheets_data.py to streamline API request handling and improve data processing

--- Day 39 - Flight Deal Finder/sheets_data.py
+++ Day 39 - Flight Deal Finder/sheets_data.py
@@ -1,35 +1,30 @@
 import requests
 import logging
 
-logging.basicConfig(
-    level=logging.INFO,
-)
+logging.basicConfig(level=logging.INFO)
 
-sheets_data = requests.get(url=f"https://api.sheety.co/2a738664cb10b9ea4971b2ff1892cbd3/flightDealData/sheet1")
+url = "https://api.sheety.co/2a738664cb10b9ea4971b2ff1892cbd3/flightDealData/sheet1"
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+response = requests.get(url, headers=headers)
+response.raise_for_status()
+
+print("Status:", response.status_code)
+
+data_json = response.json()
+
 sheets_data_list = []
-sheets_data_json = sheets_data.json()
-print(sheets_data.raise_for_status())
-print("\nSheets Data:")
-for data in sheets_data_json['sheet1']:
-    try:
-        if len(data.keys()) > 2:
-            sheets_data_list.append({
-                'destination_code' : data['destinationCode'],
-                'departure_code' : data['departureCode'],
-                'price_target' : int(data['priceTarget']),
-                'duration_target' : int(data['durationTarget']),
-                'layover_count' : data['layoverCount'],
-                'user_email' : data['emailAddress']
-            }
-            )
-            logging.info(
-            f"destination_code {data['destinationCode']} | "
-            f"departure_code {data['departureCode']} | "
-            f"price_target {int(data['priceTarget'])} | "
-            f"duration_target {int(data['durationTarget'])} | "
-            f"layover_count {data['layoverCount']} | "
-            f"user_email {data['emailAddress']}"
-            ) 
-        pass
-    except KeyError as e:
-        print(f"Error detected for route: {data['destinationCode']}-{data['departureCode']}: [{e}]")
+
+for data in data_json["sheet1"]:
+    if data.get("destinationCode"):
+        sheets_data_list.append({
+            "destination_code": data["destinationCode"],
+            "departure_code": data["departureCode"],
+            "price_target": int(data["priceTarget"]),
+            "duration_target": int(data["durationTarget"]),
+            "layover_count": data["layoverCount"],
+            "user_email": data["emailAddress"]
+        })
