a1d6f9932d735db170325f2e9fc2deed47b66049
Sahil <sahils.8877@gmail.com>
2026-04-09 22:04:18 +0100

    Update product data scraper and email alert for price drop threshold

--- Intermediate/Day 47 - Amazon Price Tracker/send_email.py
+++ Intermediate/Day 47 - Amazon Price Tracker/send_email.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 
-TARGET_PRICE = 400
-price = product_data.product_price
+TARGET_PRICE = 600
+price = product_data.product.parse_price_data()
 
 if price <= TARGET_PRICE:
     with smtplib.SMTP(host="smtp.gmail.com",port=0) as con:
         con.starttls()
-        msg = f'Subject:Amazon Price Bot!\n\nThis a Price drop alert!\n\nItem : {product_data.product_title}\n\nNew price : {price}￡'
+        msg = f'Subject:Amazon Price Bot!\n\nThis a Price drop alert!\n\nItem : {product_data.product.parse_item_name()}\n\nNew price : {product_data.product.parse_price_data()}￡'
         sender_email = os.getenv('SENDER_EMAIL')
         sender_pass = os.getenv('SENDER_EMAIL_PASS')
         receiver = 'sahils.8877@gmail.com'
