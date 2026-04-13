a27680bd6f43803414e9e0a8d79dbf360d25bf0c
Sahil <sahils.8877@gmail.com>
2026-04-13 21:33:44 +0100

    Add debug HTML upload step and restore email alert logic in send_email.py

--- Intermediate/Day 47 - Amazon Price Tracker/product_data.py
+++ Intermediate/Day 47 - Amazon Price Tracker/product_data.py
@@ -7,7 +7,7 @@ class GetProductData:
         self.session = requests.Session()
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-            "Accept-Language": "en-US,en;q=0.9",
+            "Accept-Language": "en-GB,en;q=0.8",
             "Accept-Encoding": "gzip, deflate, br",
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
             "Referer": "https://google.com",
