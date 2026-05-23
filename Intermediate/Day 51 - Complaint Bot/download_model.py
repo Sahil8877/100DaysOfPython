6c362f66a679e6d14aefe3fdd1a50127f52feadd
Sahil <sahils.8877@gmail.com>
2026-05-23 03:55:21 +0100

    Refactor model downloading process in download_model.py to use huggingface_hub for improved efficiency

--- Intermediate/Day 51 - Complaint Bot/download_model.py
+++ Intermediate/Day 51 - Complaint Bot/download_model.py
@@ -1,17 +1,13 @@
 import os
-import requests
-
-MODEL_PATH = "model/Meta-Llama-3-8B-Instruct.Q2_K.gguf"
-URL = "/Users/sahil/.cache/huggingface/hub/models--Sahil8877--complaint-bot-model/snapshots/29931d50805b7c0f707babc00c5370631caa8ac7/Meta-Llama-3-8B-Instruct.Q2_K.gguf"
+from huggingface_hub import hf_hub_download
 
 os.makedirs("model", exist_ok=True)
 
-if not os.path.exists(MODEL_PATH):
-    print("Downloading model...")
-    r = requests.get(URL, stream=True)
-    with open(MODEL_PATH, "wb") as f:
-        for chunk in r.iter_content(chunk_size=8192):
-            f.write(chunk)
-    print("Download complete")
-else:
-    print("Model already cached")
+model_path = hf_hub_download(
+    repo_id="TheBloke/Llama-3-8B-Instruct-GGUF",
+    filename="Meta-Llama-3-8B-Instruct.Q2_K.gguf",
+    local_dir="model",
+    local_dir_use_symlinks=False
+)
+
+print("Model ready at:", model_path)
