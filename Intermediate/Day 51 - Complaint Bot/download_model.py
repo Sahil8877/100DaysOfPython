f8bcd66bc1a138b8df6760bdf0c736def54d4e8e
Sahil Suryavanshi <71732134+Sahil8877@users.noreply.github.com>
2026-05-23 04:31:19 +0100

    Update download_model.py

--- Intermediate/Day 51 - Complaint Bot/download_model.py
+++ Intermediate/Day 51 - Complaint Bot/download_model.py
@@ -6,10 +6,10 @@ HF_TOKEN = os.getenv("HF_TOKEN")
 os.makedirs("model", exist_ok=True)
 
 model_path = hf_hub_download(
-    repo_id="bartowski/Llama-3.1-8B-Instruct-GGUF",
-    filename="Meta-Llama-3.1-8B-Instruct-Q2_K.gguf",
+    repo_id="bartowski/Meta-Llama-3-8B-Instruct-GGUF",
+    filename="Meta-Llama-3-8B-Instruct.Q2_K.gguf",
     token=HF_TOKEN,
     local_dir="model"
 )
 
-print("Model downloaded at:", model_path)
+print("Model ready at:", model_path)
