aa4d70fa1d2ba0a634babf96dfb7c254ab59154e
Sahil <sahils.8877@gmail.com>
2026-05-23 21:05:54 +0100

    Refactor data retrieval in main.py for clarity and maintainability

--- Intermediate/Day 51 - Complaint Bot/main.py
+++ Intermediate/Day 51 - Complaint Bot/main.py
@@ -2,13 +2,16 @@ from complain_writer import response
 from weather_checker import weather_complainer, get_weather_data, uk_cities
 from down_detector import downdetector_complainer, get_downdetector_data, companies_to_check
 from twitter import post_tweets
+down_detector_data = get_downdetector_data(companies_to_check)
+down_detector_complaints_list = downdetector_complainer(down_detector_data)
 
-down_detector_complaints_list = downdetector_complainer(get_downdetector_data(companies_to_check))
-weather_complaints_list = weather_complainer(get_weather_data(uk_cities))
+weather_data = get_weather_data(uk_cities)
+weather_complaints_list = weather_complainer(weather_data)
 
 down_detector_tweets = response(down_detector_complaints_list,"website down")
 post_tweets(down_detector_complaints_list)
 weather_tweets = response(weather_complaints_list,"weather")
 post_tweets(weather_complaints_list)
+
 print(down_detector_tweets,"\n")
 print(weather_tweets)
