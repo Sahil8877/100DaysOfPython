ea962c4c022e27920672158dcacfbb33879a7863
Sahil <sahils.8877@gmail.com>
2026-05-24 10:35:35 +0100

    Update weather data retrieval to use standardized city list

--- Intermediate/Day 51 - Complaint Bot/main.py
+++ Intermediate/Day 51 - Complaint Bot/main.py
@@ -1,12 +1,12 @@
 from complain_writer import response
-from weather_checker import weather_complainer, get_weather_data, uk_cities
+from weather_checker import weather_complainer, get_weather_data, cities
 from down_detector import downdetector_complainer, get_downdetector_data, companies_to_check
 from twitter import post_tweets
 
 down_detector_data = get_downdetector_data(companies_to_check)
 down_detector_complaints_list = downdetector_complainer(down_detector_data)
 
-weather_data = get_weather_data(uk_cities)
+weather_data = get_weather_data(cities)
 weather_complaints_list = weather_complainer(weather_data)
 
 down_detector_tweets = response(down_detector_complaints_list, "website down")
